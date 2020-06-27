6ca162aa56e2d1ceee4201d7bb11842031a05c99
changlewojing <linxiaoming@gmail.com>
2020-06-27 17:34:04 +0800

    从daxumi.net拷贝过来

--- sso-consumer/checkSSORedirect.js
+++ sso-consumer/checkSSORedirect.js
@@ -2,28 +2,36 @@ const url = require("url");
 const axios = require("axios");
 const { URL } = url;
 const { verifyJwtToken } = require("./jwt_verify");
-const validReferOrigin = "http://sso.ankuranand.com:3010";
-const ssoServerJWTURL = "http://sso.ankuranand.com:3010/simplesso/verifytoken";
+// const validReferOrigin = "http://sso.ankuranand.com:3010";
+// const ssoServerJWTURL = "http://sso.ankuranand.com:3010/simplesso/verifytoken";
+// const validReferOrigin = "http://daxumi.cn:3010";
+// const ssoServerJWTURL = "http://daxumi.cn:3010/simplesso/verifytoken";
+
+const validReferOrigin = "https://daxumi.cn/";
+const ssoServerJWTURL = "https://daxumi.cn/simplesso/verifytoken";
 
 const ssoRedirect = () => {
   return async function(req, res, next) {
     // check if the req has the queryParameter as ssoToken
     // and who is the referer.
+    console.log('checkSSORedirect0...');
     const { ssoToken } = req.query;
     if (ssoToken != null) {
       // to remove the ssoToken in query parameter redirect.
       const redirectURL = url.parse(req.url).pathname;
+      console.log('checkSSORedirect1...');
       try {
         const response = await axios.get(
           `${ssoServerJWTURL}?ssoToken=${ssoToken}`,
           {
             headers: {
-              Authorization: "Bearer l1Q7zkOL59cRqWBkQ12ZiGVW2DBL"
+              Authorization: "Bearer 1g0jJwGmRQhJwvwNOrY4i90kD0m"
             }
           }
         );
         const { token } = response.data;
         const decoded = await verifyJwtToken(token);
+        console.log('checkSSORedirect2...decoded,token',decoded,token);
         // now that we have the decoded jwt, use the,
         // global-session-id as the session id so that
         // the logout can be implemented with the global session.
@@ -31,9 +39,9 @@ const ssoRedirect = () => {
       } catch (err) {
         return next(err);
       }
-
+      console.log('checkSSORedirect3...'+`${redirectURL}`)
       return res.redirect(`${redirectURL}`);
-    }
+    } else console.log('checkSSORedirect4,ssotoken is null')
 
     return next();
   };
--- sso-consumer/isAuthenticated.js
+++ sso-consumer/isAuthenticated.js
@@ -3,12 +3,23 @@ const isAuthenticated = (req, res, next) => {
   // if not redirect the user to the SSO Server for Login
   // pass the redirect URL as current URL
   // serviceURL is where the sso should redirect in case of valid user
-  const redirectURL = `${req.protocol}://${req.headers.host}${req.path}`;
+  //const redirectURL = `${req.protocol}://${req.headers.host}${req.path}`;
+  // const redirectURL = `https://${req.headers.host}${req.path}`;
+  // 这个req.path是/，而不是we，怎么回事情，还不知道？！
+  const redirectURL = `${req.protocol}://${req.headers.host}${req.originalUrl}`;
+  // const redirectURL = `https://${req.headers.host}${req.originalUrl}`;
+  // console.log(req.url)
+  // console.log(req.originalUrl)
+  // console.log(redirectURL)
+  // console.log('135')
   if (req.session.user == null) {
     return res.redirect(
-      `http://sso.ankuranand.com:3010/simplesso/login?serviceURL=${redirectURL}`
+      `https://daxumi.cn/simplesso/login?serviceURL=${redirectURL}`
+      //`http://daxumi.cn:3010/simplesso/login?serviceURL=${redirectURL}`
+      // `http://sso.ankuranand.com:3010/simplesso/login?serviceURL=${redirectURL}`
     );
   }
+  console.log("isAuthenticated认证检查，通过！")
   next();
 };
 
